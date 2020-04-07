3c021346f71625d80b33c0369e416e61836c5475
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-04-07 14:11:04 -0600

    MVP can add more styling if you want

---
+++ my-react-app/src/comp/User.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+
+class User extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gracee: {},
+      followers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/GraceeElton")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          gracee: response.data,
+        });
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get("https://api.github.com/users/GraceeElton/followers")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          followers: response.data,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="mycard">
+        <div>
+          <div className="cardinfo">
+            <img src={this.state.gracee.avatar_url}></img>
+            <h3>{this.state.gracee.name}</h3>
+            <h3>{this.state.gracee.location}</h3>
+            <h3>{this.state.gracee.bio}</h3>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="cardinfo">
+            {this.state.followers.map((follower) => (
+              <div>
+                <img src={follower.avatar_url}></img>
+                <h2>{follower.login}</h2>
+                <h2>{follower.url}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
